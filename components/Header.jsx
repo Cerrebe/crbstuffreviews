cfacce7fca7ac7e7e146076cbf70dea0376da3ec
Cerrebe <cerrebe3000@gmail.com>
2022-05-11 22:28:50 +0200

    meta general config

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,9 +17,8 @@ const Header = () => {
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://crbstuffreviews.es/" />
         <meta property="og:image" content="/favicon.ico" />
-        <meta property="og:description" content="Site description" />
+        <meta property="og:description" content="Reviews y otras mierdas" />
         <meta name="theme-color" content="#FF0000" />
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <div className="inline-block w-full border-b border-blue-400 py-8">
         <div className="block md:float-left">
